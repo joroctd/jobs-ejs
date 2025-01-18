38f052b14be3c0734acc80dc05f7e874fa7828a2
johnoro <officialjohnorourke@gmail.com>
2025-01-18 14:00:39 -0500

    sets up mongo session

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 import express from 'express';
 import 'express-async-errors';
-import connectDatabase from './db/connect';
+import session from 'express-session';
+import connectMongoSession from 'connect-mongodb-session';
+import flash from 'connect-flash';
+import connectDatabase from './db/connect.js';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+if (app.get('env') === 'development') {
+	process.loadEnvFile('./.env');
+}
+
+const MongoDBStore = connectMongoSession(session);
+const store = new MongoDBStore({
+	uri: process.env.MONGO_URI,
+	collection: 'mySessions'
+});
+store.on('error', error => {
+	console.error(error);
+});
+const sessionParams = {
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true,
+	store,
+	cookie: {
+		secure: false,
+		sameSite: 'strict'
+	}
+};
+
+if (app.get('env') === 'production') {
+	app.set('trust proxy', 1);
+	sessionParams.cookie.secure = true;
+}
+
+app.use(session(sessionParams));
+app.use(flash());
 
 // secret word handling
 let secretWord = 'syzygy';
