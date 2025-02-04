f5e35daf370ebf1752058cdcb12385677ee141a1
johnoro <officialjohnorourke@gmail.com>
2025-02-03 19:17:27 -0500

    adds sessions

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 import express from 'express';
 import 'express-async-errors';
-import connectDatabase from './db/connect';
+
+import session from 'express-session';
+import connectMongoSession from 'connect-mongodb-session';
+import flash from 'connect-flash';
+
+import connectDatabase from './db/connect.js';
 
 const app = express();
+
+if (app.get('env') === 'development') {
+	process.loadEnvFile('./.env');
+}
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-// secret word handling
-let secretWord = 'syzygy';
+const MongoDBStore = connectMongoSession(session);
+const store = new MongoDBStore({
+	uri: process.env.MONGO_URI,
+	collection: 'mySessions'
+});
+store.on('error', console.error);
+const sessionParams = {
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true,
+	store,
+	cookie: {
+		secure: false,
+		sameSite: 'strict'
+	}
+};
+
+if (app.get('env') === 'production') {
+	sessionParams.cookie.secure = true;
+}
+
+app.use(session(sessionParams));
+app.use(flash());
+
 app.get('/secretWord', (req, res) => {
-	res.render('secretWord', { secretWord });
+	if (!req.session.secretWord) {
+		req.session.secretWord = 'syzygy';
+	}
+
+	res.render('secretWord', { secretWord: req.session.secretWord });
 });
 app.post('/secretWord', (req, res) => {
-	secretWord = req.body.secretWord;
+	req.session.secretWord = req.body.secretWord;
 	res.redirect('/secretWord');
 });
 
--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = url => {
 	return mongoose.connect(url);
